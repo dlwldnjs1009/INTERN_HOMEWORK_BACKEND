efecc58cd62328a642a25c5377f7a60714fcf894
LeeJiWon <dlwldnjs1009@gmail.com>
2025-03-11 17:57:14 +0900

    chore: @Getter 어노테이션 추가, createAt 필드명 변경

--- HOMEWORK_BACK/src/main/java/com/example/homework_back/common/config/db/BaseEntity.java
+++ HOMEWORK_BACK/src/main/java/com/example/homework_back/common/config/db/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.hibernate.annotations.SoftDelete;
 import org.hibernate.type.YesNoConverter;
 import org.springframework.data.annotation.CreatedDate;
@@ -12,12 +13,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 @SoftDelete(columnName = "deleted", converter = YesNoConverter.class)
 public class BaseEntity {
 
     @CreatedDate
     @Column(name = "create_at",updatable = false)
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at")
