3f18a0e4d7af4d6c5564517401ca0d7e4dcb40fb
LeeJiWon <dlwldnjs1009@gmail.com>
2025-03-12 10:35:02 +0900

    feat: responseDto에 권한ID List로 넣을 수 있는 기능

--- HOMEWORK_BACK/src/main/java/com/example/homework_back/role/dto/response/RoleResponseDto.java
+++ HOMEWORK_BACK/src/main/java/com/example/homework_back/role/dto/response/RoleResponseDto.java
@@ -1,7 +1,9 @@
 package com.example.homework_back.role.dto.response;
 
+import com.example.homework_back.permission.entity.Permission;
 import com.example.homework_back.role.entity.RoleType;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
@@ -9,14 +11,16 @@ public class RoleResponseDto {
     private final Long id;
     private final RoleType name;
     private final String description;
+    private final List<Long> permissionId;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 
-    public RoleResponseDto(Long id, RoleType name, String description, LocalDateTime createdAt,
+    public RoleResponseDto(Long id, RoleType name, String description, List<Long> permissionId, LocalDateTime createdAt,
             LocalDateTime updatedAt) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.permissionId = permissionId;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
--- HOMEWORK_BACK/src/main/java/com/example/homework_back/role/mapper/RoleMapper.java
+++ HOMEWORK_BACK/src/main/java/com/example/homework_back/role/mapper/RoleMapper.java
@@ -1,16 +1,22 @@
 package com.example.homework_back.role.mapper;
 
+import com.example.homework_back.permission.entity.Permission;
 import com.example.homework_back.role.dto.request.RoleRequestDto;
 import com.example.homework_back.role.dto.request.RoleUpdateRequestDto;
 import com.example.homework_back.role.dto.response.RoleResponseDto;
 import com.example.homework_back.role.entity.Role;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
 
+    @Mapping(target = "permissionId", source = "permissions", qualifiedByName = "streamPermissions")
     RoleResponseDto toRoleResponseDto(Role role);
 
     Role toRoleForCreate(RoleRequestDto roleRequestDto);
@@ -23,5 +29,12 @@ public interface RoleMapper {
                 dto.getName(),
                 dto.getDescription()
         );
+    }
+
+    @Named("streamPermissions")
+    default List<Long> streamPermissions(List<Permission> permissions) {
+        return permissions.stream()
+                .map(Permission::getId)
+                .collect(Collectors.toList());
     }
 }
