91a832d3f1115d71b5ee3ff25487cb8785b9e937
LeeJiWon <dlwldnjs1009@gmail.com>
2025-03-12 10:32:35 +0900

    fix: role permission 연관관계 단방향

--- HOMEWORK_BACK/src/main/java/com/example/homework_back/role/entity/Role.java
+++ HOMEWORK_BACK/src/main/java/com/example/homework_back/role/entity/Role.java
@@ -38,12 +38,19 @@ public class Role extends BaseEntity {
     @Column(name = "role_description")
     private String description;
 
-     @OneToMany(mappedBy = "role")
-     private List<Users> users = new ArrayList<>();
+    @OneToMany(mappedBy = "role")
+    private List<Users> users = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.LAZY) // 기본 패치 전략은 지연 로딩
+    @JoinTable(name = "role_permission", // JPA는 내부적으로 "role_permission" 조인 테이블 생성
+            joinColumns = @JoinColumn(name = "role_id"), // role_id와 permission_id를 결합한 유니크 제약 조건으로 관리
+            inverseJoinColumns = @JoinColumn(name = "permission_id")) // 두 컬럼의 조합은 유일하게 관리됨.
+    private List<Permission> permissions = new ArrayList<Permission>();
 
     public Role(RoleType name, String description) {
         this.name = name;
         this.description = description;
+//        this.permissions = permissionxs;
     }
 
     public void updateFromDto(RoleType name, String description) {
